8dc632e114c32c95013c9e98f33db0d85545b0ed
Predator333 <jorgeyolijavi@gmail.com>
2025-08-07 19:43:09 +0200

    Mis primeras consultas resueltas

--- Bases de datos/MIS PRIMERAS CONSULTAS.sql
+++ Bases de datos/MIS PRIMERAS CONSULTAS.sql
@@ -1,24 +1,24 @@
 select avg(alt) from circuits;
 select country as pais, count(*) as num_circuitos from circuits group by country order by num_circuitos Desc;
 SELECT name from circuits where country="France";
-select name from circuits order by asc;
+select name from circuits order by name;
 select location, country from circuits where alt>"500";
-select circuitRef as ubicacion;
-select name from circuits where country in ('Australia', 'Austria', 'Spain', 'Portugal', 'France', 'Netherland', 'United_Kingdom', 'Germany', 'Monaco', 'Hungary', 'Belgium', 'Italy');
+select circuitRef as ubicacion from circuits where lat= (select min(lat) from circuits);
+select name, country from circuits where country in ('Austria', 'Spain', 'Portugal', 'France', 'Netherlands', 'United_Kingdom', 'Germany', 'Monaco', 'Hungary', 'Belgium', 'Italy', 'Switzerland' );
 select name from circuits where name like '%Grand%';
 select country, count(*) as num_circuitos from circuits group by country;
-select * from circuits where alt <(select avg(alt) from circuits);
-select * from circuits where country in (select country from circuits group by country having count(country) > 3 );
-select alt, lat from circuits where country in ('Japan');
-
-select circuitRef from circuits where lat < "-25";
+select name, alt from circuits where alt <(select avg(alt) from circuits);
+select name from circuits where country in (select country from circuits group by country having count(country) > 3 );
+select name, lng, lat from circuits where country in ('Japan');
+select name, url from circuits where url is not null;
+select name from circuits order by alt asc limit 1;
 SELECT location, COUNT(*) AS circuits FROM circuits GROUP BY location ORDER BY circuits DESC;
-select name, circuitRef from circuits where circuitRef= name;
-select * from circuits where alt is not null;
-select name from circuits where substring(name, 1,1) ='S';
-select circuitRef as ubicacion;
-
-
-
-select name from circuits where name like '%Circuit%';
-select name from circuits where name like '%circuit%';
+select name, location from circuits where location= name;
+select name  from circuits where alt is null;
+select name from circuits where name like 'S%';
+select name, alt, location from circuits order by alt desc limit 5;
+select name, url, alt from circuits where alt >(select avg(alt) from circuits);
+select location, count(*) as num_circuitos from circuits group by location HAVING COUNT(*) >1;
+select name, location, lat from circuits where lat between 40 and 50;
+select name from circuits where name like 'Circuit%';
+ select name from circuits where lower(name) like '%circuit';
